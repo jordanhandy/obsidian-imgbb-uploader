8414eee29522f3203668c7afd0085fe94d5f07ce
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2024-06-24 17:10:06 -0400

    get upload result

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import axios from "axios";
 const electron = require('electron');
 export default class ImgBBUploader extends Plugin {
 	settings: ImgBBSettings;
+	
+	private clearHandlers(): void {
+		this.app.workspace.off('editor-paste', this.pasteHandler);
+		this.app.workspace.off('editor-drop', this.dropHandler);
+	  }
 
 	private setupHandlers(): void {
 		if (this.settings.clipboard) {
@@ -29,36 +34,70 @@ export default class ImgBBUploader extends Plugin {
 		await this.uploadFiles(files, event, editor); // to fix
 	}
 
-	private async uploadFiles(files : FileList,event,editor){
+	private isType(file: File): boolean {
+		let isValid = false;
+		for (let ext of supportedExtensions) {
+			if (file.name.endsWith(ext));
+			isValid = true;
+			return isValid;
+		}
+		return isValid;
+	}
+	private async uploadFiles(files: FileList, event, editor) {
 		let formParams;
-		if(files.length > 0 && isType){
-			for(let file of files){
-				if(this.settings.expiration){
-					formParams = {
-						key: this.settings.apiKey,
-						expiration: this.settings.expirationTime
+		if (files.length > 0) {
+			for (let file of files) {
+				if (this.isType(file)) {
+					const randomString = (Math.random() * 10086).toString(36).substr(0, 8)
+            const pastePlaceText = `![uploading...](${randomString})\n`
+            editor.replaceSelection(pastePlaceText)
+					if (this.settings.expiration) {
+						formParams = {
+							key: this.settings.apiKey,
+							expiration: this.settings.expirationTime
+						}
+					} else {
+						formParams = {
+							key: this.settings.apiKey,
+						}
 					}
-				}else{
-					formParams = {
-						key: this.settings.apiKey,
-					}
+					let formData = new FormData();
+					formData.append('image', file);
+					// Make API call
+					axios({
+						url: 'https://api.imgbb.com/upload',
+						method: 'POST',
+						data: formData,
+						params: formParams
+					}).then((res)=>{
+						console.log(res);
+					})
+
 				}
-				let formData = new FormData();
-				formData.append('image',file);
-				// Make API call
-				axios({
-					url:'https://api.imgbb.com/upload',
-					method: 'POST',
-					data: formData,
-					params: formParams
-				})
-				
 			}
 
 		}
 	}
 
 
+
+	  // Function to replace text
+	  private replaceText(editor: Editor, target: string, replacement: string): void {
+		target = target.trim();
+		let lines = [];
+		for (let i = 0; i < editor.lineCount(); i++) {
+		  lines.push(editor.getLine(i));
+		}
+		for (let i = 0; i < lines.length; i++) {
+		  const ch = lines[i].indexOf(target)
+		  if (ch !== -1) {
+			const from = { line: i, ch };
+			const to = { line: i, ch: ch + target.length };
+			editor.replaceRange(replacement, from, to);
+			break;
+		  }
+		}
+	  }
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new ImgBBUploaderSettingsTab(this.app, this));
@@ -73,10 +112,10 @@ export default class ImgBBUploader extends Plugin {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-  // When saving settings
-  async saveSettings(): Promise<void> {
-    await this.saveData(this.settings);
-    this.clearHandlers();
-    this.setupHandlers();
-  }
+	// When saving settings
+	async saveSettings(): Promise<void> {
+		await this.saveData(this.settings);
+		this.clearHandlers();
+		this.setupHandlers();
+	}
 }
