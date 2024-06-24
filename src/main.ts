3ae5e14764131fb667e9ce667406701ea9c15cdc
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2024-06-24 16:34:19 -0400

    logging

--- src/main.ts
+++ src/main.ts
@@ -33,56 +33,18 @@ export default class ImgBBUploader extends Plugin {
 	}
 
 	onunload() {
-
+		console.log('unloading imgBB Uploader...');
 	}
 
 	async loadSettings() {
+		console.log('Loading imgBB Uploader...');
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  // When saving settings
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
+    this.clearHandlers();
+    this.setupHandlers();
+  }
 }
